67737ca473833d3f11fc3a88404efd3a844c0a52
Pankaj Bhatta <coniopankaj999@gmail.com>
2024-07-05 15:31:21 +0545

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  loginForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
